5a6a3ac2c44d0c161a29bb3517d58dd48e523d8d
AmitThakkar <vigildbest@gmail.com>
2016-08-22 18:16:52 +0530

    adding bubble sort

---
+++ bubble.sort.js
@@ -0,0 +1,18 @@
+/**
+ * Created by amitthakkar on 22/08/16.
+ */
+let bubbleSort = (array) => {
+    for (let index = 0; index < array.length; index++) {
+        for (let index2 = index + 1; index2 < array.length; index2++) {
+            if (array[index] > array[index2]) {
+                let temp = array[index];
+                array[index] = array[index2];
+                array[index2] = temp;
+            }
+        }
+    }
+};
+
+let array = [5, 4, 8, 3, 2, 1];
+bubbleSort(array);
+console.log(array);
