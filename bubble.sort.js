ec0ec90c9c9f3e61d0afb2e702b1d393bed241ae
AmitThakkar <vigildbest@gmail.com>
2016-08-23 11:47:17 +0530

    adding complexity for bubble sort

--- bubble.sort.js
+++ bubble.sort.js
@@ -1,6 +1,10 @@
 /**
  * Created by amitthakkar on 22/08/16.
  */
+/*
+ * Space Complexity O(1)
+ * Time Complexity n*n while actual is n(n-1)/2
+ * */
 let bubbleSort = (array) => {
     for (let index = 0; index < array.length; index++) {
         for (let index2 = index + 1; index2 < array.length; index2++) {
