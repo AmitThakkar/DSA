dd470ef5f7388efb65e76da405b604d3620639b2
AmitThakkar <vigildbest@gmail.com>
2016-09-23 15:26:02 +0530

    adding total sum combination logic.

---
+++ sum.combinations.js
@@ -0,0 +1,26 @@
+/**
+ * Created by amitthakkar on 23/09/16.
+ */
+(() => {
+    let numbers = [1, 2, 3];
+    let sum = 4;
+    let results = [];
+
+    function getAllCombinations(numbers, sum, result) {
+        if (sum == 0) {
+            count++;
+            results.push(result);
+            return;
+        } else if (sum < 0) {
+            return;
+        }
+        for (let index = 0; index < numbers.length; index++) {
+            let tempResult = result.slice();
+            tempResult.push(numbers[index]);
+            getAllCombinations(numbers, sum - numbers[index], tempResult);
+        }
+    }
+
+    getAllCombinations(numbers, sum, []);
+    console.log(results);
+})();
