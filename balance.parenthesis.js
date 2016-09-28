ee2310b9479c3f15e8f4ba816e30ac971d3ffc70
AmitThakkar <vigildbest@gmail.com>
2016-09-28 17:29:37 +0530

    adding balance parenthesis logic.

---
+++ balance.parenthesis.js
@@ -0,0 +1,50 @@
+/**
+ * Created by amitthakkar on 28/09/16.
+ */
+(() => {
+    let isBalanceParenthesis = (string) => {
+        let stack = [], isBalance = true, temp;
+        for (let index = 0; index < string.length && isBalance; index++) {
+            switch (string[index]) {
+                case '(':
+                    stack.push('(');
+                    break;
+                case ')':
+                    temp = stack.pop();
+                    if (temp != '(') {
+                        isBalance = false;
+                    }
+                    break;
+                case '{':
+                    stack.push('{');
+                    break;
+                case '}':
+                    temp = stack.pop();
+                    if (temp != '{') {
+                        isBalance = false;
+                    }
+                    break;
+                case '[':
+                    stack.push('[');
+                    break;
+                case ']':
+                    temp = stack.pop();
+                    if (temp != '[') {
+                        isBalance = false;
+                    }
+                    break;
+            }
+        }
+        if(stack.length > 0) {
+            isBalance = false;
+        }
+        return isBalance;
+    };
+
+    console.log(isBalanceParenthesis("a(bcd)d")); // true
+    console.log(isBalanceParenthesis("(kjds(hfkj)sdhf")); // false
+    console.log(isBalanceParenthesis("(sfdsf)(fsfsf")); // false
+    console.log(isBalanceParenthesis("{[]}()")); // true
+    console.log(isBalanceParenthesis("{[}]")); // true
+    console.log(isBalanceParenthesis("{}]")); // false
+})();
