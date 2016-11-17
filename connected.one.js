b59ffce92d51307c20458f52856decc0d7f06e50
AmitThakkar <vigildbest@gmail.com>
2016-11-17 10:08:07 +0530

    Adding solution of connectedOnes.

--- connected.one.js
+++ connected.one.js
@@ -21,11 +21,42 @@
         [1, 1, 1, 0]
     ];
 
-    let maxConnectedOne = (input) => {
-
+    let getConnectedOneCount = (array, length, width, i, j) => {
+        let count = 1;
+        if (j + 1 < width && array[i][j + 1] === 1) {
+            array[i][j + 1] = 0;
+            count += getConnectedOneCount(array, length, width, i, j + 1);
+        }
+        if (i + 1 < length && array[i + 1][j] === 1) {
+            array[i + 1][j] = 0;
+            count += getConnectedOneCount(array, length, width, i + 1, j);
+        }
+        if (i + 1 < length && j + 1 < width && array[i + 1][j + 1] === 1) {
+            array[i + 1][j + 1] = 0;
+            count += getConnectedOneCount(array, length, width, i + 1, j + 1);
+        }
+        return count;
     };
 
-    maxConnectedOne(array1); // 6
-    maxConnectedOne(array2); // 4
-    maxConnectedOne(array3); // 4
+    let maxConnectedOne = (input) => {
+        let length = input.length;
+        let width = input[0].length;
+        let maxConnectedOnesCount = 0;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < width; j++) {
+                if (input[i][j] === 1) {
+                    input[i][j] = 0;
+                    let count = getConnectedOneCount(input, length, width, i, j);
+                    if (count > maxConnectedOnesCount) {
+                        maxConnectedOnesCount = count;
+                    }
+                }
+            }
+        }
+        return maxConnectedOnesCount;
+    };
+
+    console.log(maxConnectedOne(array1)); // 6
+    console.log(maxConnectedOne(array2)); // 5
+    console.log(maxConnectedOne(array3)); // 4
 })();
