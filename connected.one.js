435ae5e53a5decaa9baf8faa684f1cad5ad3c48a
AmitThakkar <vigildbest@gmail.com>
2016-11-17 09:48:58 +0530

    Adding problem.

---
+++ connected.one.js
@@ -0,0 +1,31 @@
+/**
+ * Created by amitthakkar on 17/11/16.
+ */
+(() => {
+    let array1 = [
+        [1, 1, 1, 0],
+        [1, 0, 1, 0],
+        [0, 0, 1, 0],
+        [1, 0, 0, 0]
+    ];
+    let array2 = [
+        [1, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 1, 0],
+        [1, 0, 0, 0]
+    ];
+    let array3 = [
+        [1, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 0]
+    ];
+
+    let maxConnectedOne = (input) => {
+
+    };
+
+    maxConnectedOne(array1); // 6
+    maxConnectedOne(array2); // 4
+    maxConnectedOne(array3); // 4
+})();
