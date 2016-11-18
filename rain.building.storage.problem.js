f7c76ae5f179cf40ff7f6920ff6382bfe353fde0
AmitThakkar <vigildbest@gmail.com>
2016-11-18 10:36:46 +0530

    Adding Problem.

--- rain.building.storage.problem.js
+++ rain.building.storage.problem.js
@@ -1,9 +1,28 @@
 /**
  * Created by amitthakkar on 17/11/16.
  */
+/*
+ * Problem: You are given an input array whose each element represents the height of a line towers. The width of every
+ * tower is 1. It starts raining. How much water is collected between the towers?
+ *
+ * Input: [1,5,3,7,2] , Output: 2 units
+ * Explanation: 2 units of water collected between towers of height 5 and 7
+ *
+ *
+ *
+ *     *
+ *     *
+ *   *W*
+ *   *W*
+ *   ***
+ *   ****
+ *  ******
+ * */
+
 (() => {
     // let buildingHeights = [6, 1, 3, 5, 9, 2, 8];
-    let buildingHeights = [5, 3, 7, 2, 6, 4, 5, 9, 1, 2];
+    // let buildingHeights = [5, 3, 7, 2, 6, 4, 5, 9, 1, 2];
+    let buildingHeights = [1,5,3,7,2];
     /*
      * TOTAL store water
      * */
@@ -27,7 +46,7 @@
         totalStorage += storeWater;
         tempStorage += storeWater;
         if (storeWater === 0) {
-            if (tempStorage > maxStorageBetween2Building) {
+            if (tempStorage >= maxStorageBetween2Building) {
                 maxStorageBetween2Building = tempStorage;
                 tempStorage = 0;
                 leftBuilding = rightBuilding;
