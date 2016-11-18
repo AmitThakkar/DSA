39b5ba493bd77c7f8a36a45470167de736c2cf51
AmitThakkar <vigildbest@gmail.com>
2016-11-18 10:25:48 +0530

    adding rain building storage problem.

--- rain.building.storage.problem.js
+++ rain.building.storage.problem.js
@@ -2,7 +2,8 @@
  * Created by amitthakkar on 17/11/16.
  */
 (() => {
-    let buildingHeights = [6, 1, 3, 5, 9, 2, 8];
+    // let buildingHeights = [6, 1, 3, 5, 9, 2, 8];
+    let buildingHeights = [5, 3, 7, 2, 6, 4, 5, 9, 1, 2];
     /*
      * TOTAL store water
      * */
@@ -20,9 +21,17 @@
     for (let i = buildingHeights.length - 1; i >= 0; i--) {
         maxHeightFromRight[i] = max(buildingHeights[i], i < (buildingHeights.length - 1) ? maxHeightFromRight[i + 1] : 0);
     }
-    let totalStorage = 0;
+    let totalStorage = 0, maxStorageBetween2Building = 0, tempStorage = 0;
     for (let i = 0; i < buildingHeights.length; i++) {
-        totalStorage += min(maxHeightFromLeft[i], maxHeightFromRight[i]) - buildingHeights[i];
+        let storeWater = min(maxHeightFromLeft[i], maxHeightFromRight[i]) - buildingHeights[i];
+        totalStorage += storeWater;
+        tempStorage += storeWater;
+        if (storeWater === 0) {
+            if (tempStorage > maxStorageBetween2Building) {
+                maxStorageBetween2Building = tempStorage;
+                tempStorage = 0;
+            }
+        }
     }
-    console.log(totalStorage);
+    console.log(totalStorage, maxStorageBetween2Building);
 })();
