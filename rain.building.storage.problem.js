c7fdb2511db35c947fced64e19541f2656863920
AmitThakkar <vigildbest@gmail.com>
2016-11-18 09:48:50 +0530

    adding rain building storage problem.

--- rain.building.storage.problem.js
+++ rain.building.storage.problem.js
@@ -3,19 +3,26 @@
  */
 (() => {
     let buildingHeights = [6, 1, 3, 5, 9, 2, 8];
-    let maxStoreWater = 0;
-    let firstBuilding, secondBuilding;
-    for (let i = 0; i < buildingHeights.length - 2; i++) {
-        for (let j = 2; j < buildingHeights.length; j++) {
-            let fb = buildingHeights[i];
-            let sb = buildingHeights[j];
-            let storage = (fb - sb) * (j - i - 1);
-            if (storage > maxStoreWater) {
-                maxStoreWater = storage;
-                firstBuilding = fb;
-                secondBuilding = sb;
-            }
-        }
+    /*
+     * TOTAL store water
+     * */
+    let max = (n1, n2) => {
+        return n1 > n2 ? n1 : n2;
+    };
+    let min = (n1, n2) => {
+        return n1 > n2 ? n2 : n1;
+    };
+
+    let maxHeightFromLeft = {}, maxHeightFromRight = {};
+    for (let i = 0; i < buildingHeights.length; i++) {
+        maxHeightFromLeft[i] = max(buildingHeights[i], (i != 0) ? maxHeightFromLeft[i - 1] : 0);
     }
-    console.log(firstBuilding, secondBuilding, maxStoreWater)
+    for (let i = buildingHeights.length - 1; i >= 0; i--) {
+        maxHeightFromRight[i] = max(buildingHeights[i], i < (buildingHeights.length - 1) ? maxHeightFromRight[i + 1] : 0);
+    }
+    let totalStorage = 0;
+    for (let i = 0; i < buildingHeights.length; i++) {
+        totalStorage += min(maxHeightFromLeft[i], maxHeightFromRight[i]) - buildingHeights[i];
+    }
+    console.log(totalStorage);
 })();
