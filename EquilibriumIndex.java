577588789c95ff2301880b1f5a2693f9c7ac246c
AmitThakkar <vigildbest@gmail.com>
2017-07-15 09:04:45 +0530

    Adding problem statement.

--- EquilibriumIndex.java
+++ EquilibriumIndex.java
@@ -1,3 +1,30 @@
+/*
+* Sum of zero elements is assumed to be equal to 0. This can happen if P = 0 or if P = N−1.
+*
+* For example, consider the following array A consisting of N = 8 elements:
+*
+* A[0] = -1
+* A[1] =  3
+* A[2] = -4
+* A[3] =  5
+* A[4] =  1
+* A[5] = -6
+* A[6] =  2
+* A[7] =  1
+*
+* P = 1 is an equilibrium index of this array, because:
+*
+* A[0] = −1 = A[2] + A[3] + A[4] + A[5] + A[6] + A[7]
+* P = 3 is an equilibrium index of this array, because:
+*
+* A[0] + A[1] + A[2] = −2 = A[4] + A[5] + A[6] + A[7]
+* P = 7 is also an equilibrium index, because:
+*
+* A[0] + A[1] + A[2] + A[3] + A[4] + A[5] + A[6] = 0
+* and there are no elements with indices greater than 7.
+*
+* */
+
 import java.util.ArrayList;
 
 public class EquilibriumIndex {
